b1a2d665c791e04430f90c2145438d74a824efca
Chinmay Joshi <chinmay0508@gmail.com>
2026-02-26 08:45:58 +0530

    Instantiated the immediate generator in the top module

--- top/top.v
+++ top/top.v
@@ -4,7 +4,7 @@ module top(
 );
 
 //32 bit wire that connects the PC output to the Instruction Memory input 
-wire [31:0] PC_IM;
+wire [31:0] PC_IM, instr_top;
 // instantiating the PC
 PC PC(
     .pc_in(),
@@ -16,6 +16,26 @@ PC PC(
 // instantiating the Instruction Memory
 InstructionMemory InstructionMemory(
     .address_read(PC_IM),
-    .instruction_out()
+    .instruction_out(instr_top)
 );
+
+// instantiating the Register File 
+registerFile registerFile(
+    .clk(clk),
+    .rst(rst),
+    .rs1(instr_top[19:15]),
+    .rs2(instr_top[24:20]),
+    .rd(instr_top[11:7]),
+    .regWrite(),
+    .writeData(),
+    .readData1(),
+    .readData2(),
+);
+
+// instantiating the Immediate Generator
+immediateGenerator immediateGenerator(
+        .instr_in(instr_top),
+        .immediate_instr()
+    );
+
 endmodule
